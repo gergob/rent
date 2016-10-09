b3f15b132d61700f85a712264e35e3f0b75b08b3
gergo.bogdan@accedo.tv <greg@grelution.com>
2016-10-09 21:37:46 +0200

    added react native elements and changed the default page of both android and iOS applications

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 
 class App extends Component {
   render() {
     return (
-      <View >
-        <Text >
-          Welcome to React Native!
+      // implemented with Text and Button as children
+      <Card
+        title='HELLO WORLD'
+        image={require('../images/accedo.png')}
+        imageStyle={{height:90}}>
+        <Text style={{marginBottom: 10}}>
+          The idea with React Native Elements is more about component structure than actual design.
         </Text>
-        <Text >
-          To get started, edit index.android.js or index.ios.js
-        </Text>
-        <Text >
-          Double tap R on your keyboard to reload,{'\n'}
-          Cmd + R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <Button
+          small
+          icon={{name: 'code'}}
+          backgroundColor='#03A9F4'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title='VIEW NOW' />
+      </Card>
     );
   }
 }
