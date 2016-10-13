86f6a44bd2d7e86a667cca8f5290cde7b1bf5288
gergo.bogdan@accedo.tv <greg@grelution.com>
2016-10-13 10:58:20 +0200

    added loading animation

--- src/components/pages/Loading.js
+++ src/components/pages/Loading.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import  {
     View,
-    Text,
-    Alert,
-    TextInput,
-    Image,
-    TouchableHighlight,
+    ActivityIndicator,
     StyleSheet
 } from 'react-native';
 
 
 
 const Loading = React.createClass({
-    getInitialState() {
-        return {
-
-        }
-    },
-
     render () {
         return (
-            <Text>Loading</Text>
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              flexDirection: 'column',
+              justifyContent: 'center'
+            }}>
+              <ActivityIndicator
+                size="large"
+                color="#00A4E4"
+              />
+          </View>
         );
     }
 });
