0bad2c3fc947ee21042e45f79bc676f41840f6f8
gergo.bogdan@accedo.tv <greg@grelution.com>
2016-10-13 10:34:06 +0200

    Added dynamic menu generation based on API response

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import UserManager from './UserManager';
 import Login from './components/Login';
 import NavBar from './components/NavBar';
 
-const apiBaseUrl = 'http://192.168.1.183:8080/';
+const apiBaseUrl = 'http://192.168.0.11:8080/';
 const userManager = new UserManager(apiBaseUrl);
 
 const ROUTES = [
-  { index: 0, 'comp': Login},
-  { index: 1, 'comp': NavBar}
+  { index: 0, 'comp': Login, name:'login'},
+  { index: 1, 'comp': NavBar, name: 'navbar'}
 ];
 
 const styles = StyleSheet.create({
@@ -23,21 +23,41 @@ const styles = StyleSheet.create({
 
 class App extends Component {
     renderScene (route, navigator) {
-        let Component = ROUTES[route.index].comp;
-        return <Component route={route} navigator={navigator} userManager={userManager} routes={ROUTES}/>;
-      }
+      let routeElement = ROUTES[route.index];
+      let Component = routeElement.comp;
 
-      render () {
-         return (
-             <Navigator
-               routeStack= {ROUTES}
-               style={ styles.container }
-               initialRoute={ ROUTES[0] }
-               renderScene={this.renderScene}
-               configureScene={ () => { return Navigator.SceneConfigs.FloatFromRight; } }
-             />
-         );
+      if(routeElement.name === 'navbar') {
+        return (
+          <Component
+            route={ route }
+            navigator={ navigator }
+            apiBaseUrl={ apiBaseUrl }
+          />
+        );
       }
+      else if (routeElement.name === 'login') {
+        return (
+          <Component
+            route={ route }
+            navigator={ navigator }
+            userManager={ userManager }
+            routes={ ROUTES }
+          />
+        );
+      }
+    }
+
+    render () {
+       return (
+        <Navigator
+          routeStack= { ROUTES }
+          style={ styles.container }
+          initialRoute={ ROUTES[1] }
+          renderScene={ this.renderScene }
+          configureScene={ () => { return Navigator.SceneConfigs.FloatFromRight; } }
+        />
+      );
+    }
 }
 
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,15 @@ import { Tabs, Tab } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Resume from './pages/Resume';
+import Loading from './pages/Loading';
+
+//
+// The page names are coming from the API call, this is part of the protocol
+//
+const pageMapping = {
+  'Resume': Resume,
+  'Loading': Loading
+};
 
 let styles = StyleSheet.create({
   titleStyle: {
@@ -17,66 +26,92 @@ let styles = StyleSheet.create({
 
 export default class NavBar extends Component {
   constructor () {
-      super()
+      super();
       this.state = {
-        selectedTab: 'home'
+        selectedTab: 'loading',
+        menuItems:[{
+          'action': 'accedo://page/loading',
+          'id': 'loading',
+          'state': '',
+          'title': 'Loading',
+          'page': 'Loading',
+          'icon': 'cloud-download',
+          'items': []
+        }]
       }
       this.changeTab = this.changeTab.bind(this)
     }
+
+    componentWillMount () {
+      console.info('NavBar - componentWillMount() invoked.');
+      let endpoint = this.props.apiBaseUrl + 'api/menu';
+      let self = this;
+      console.info('NavBar - fetching menu details from API.');
+      console.info('NavBar - API Endpoint is: [' + endpoint + ']');
+      fetch(endpoint, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }})
+        .then((response) => {
+          console.info('NavBar - Response from API arrived.');
+          if(response.ok) {
+            response.json()
+              .then((menu) => {
+                console.info('NavBar - Parsed data to JSON.');
+                let menuItems = menu.config_menu.items;
+                let selectedTab = menuItems[0].id; // this should be resume
+                self.setState({
+                  selectedTab: selectedTab,
+                  menuItems: menuItems
+                });
+              });
+          }
+          else {
+            console.error('NavBar - Fetching from API failed, status:' + response.status);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          Alert.alert(
+            'Error',
+            error);
+        });
+    }
+
     changeTab (selectedTab) {
       this.setState({
         selectedTab
       })
     }
 
-    render() {
+    render () {
       const { toggleSideMenu } = this.props
-      const { selectedTab } = this.state
-      return (
-        <Tabs hidesTabTouch>
+      const { selectedTab, menuItems } = this.state
+      console.info('NavBar - render() invoked.');
+
+      let innerTabs = menuItems.map((menuItem) => {
+        var Component = pageMapping[menuItem.page];
+        //console.warn('Mapped page value is:' + MappedPage);
+        return (
           <Tab
+            key={ menuItem.id }
             titleStyle={[styles.titleStyle]}
             selectedTitleStyle={[styles.titleSelected, {marginTop: -3, marginBottom: 7}]}
-            selected={selectedTab === 'home'}
-            title={selectedTab === 'home' ? 'HOME' : null}
-            renderIcon={() => <Icon color={'grey'} name='whatshot' size={26} />}
-            renderSelectedIcon={() => <Icon color={'blue'} name='whatshot' size={26} />}
-            onPress={() => this.changeTab('home')}>
-            <Resume />
+            selected={selectedTab === menuItem.id }
+            title={selectedTab === menuItem.id ? menuItem.title : null}
+            renderIcon={() => <Icon color={'grey'} name={menuItem.icon} size={26} />}
+            renderSelectedIcon={() => <Icon color={'#00A4E4'} name={menuItem.icon} size={26} />}
+            onPress={() => this.changeTab(menuItem.id)}>
+            <Component />
           </Tab>
-          <Tab
-            tabStyle={selectedTab !== 'about' && { marginBottom: -6 }}
-            titleStyle={[styles.titleStyle, {marginTop: -1}]}
-            selectedTitleStyle={[styles.titleSelected, {marginTop: -3, marginBottom: 7}]}
-            selected={selectedTab === 'about'}
-            title={selectedTab === 'about' ? 'ABOUT' : null}
-            renderIcon={() => <Icon style={{paddingBottom: 4}} color={'grey'} name='important-devices' size={26} />}
-            renderSelectedIcon={() => <Icon color={'blue'} name='important-devices' size={26} />}
-            onPress={() => this.changeTab('about')}>
-                <Resume />
-          </Tab>
-          <Tab
-            tabStyle={selectedTab !== 'contact' && { marginBottom: -6 }}
-            titleStyle={[styles.titleStyle, {marginTop: -1}]}
-            selectedTitleStyle={[styles.titleSelected, {marginTop: -3, marginBottom: 7}]}
-            selected={selectedTab === 'contact'}
-            title={selectedTab === 'contact' ? 'CONTACT' : null}
-            renderIcon={() => <Icon style={{paddingBottom: 4}} color={'grey'} name='contacts' size={26} />}
-            renderSelectedIcon={() => <Icon color={'blue'} name='contacts' size={26} />}
-            onPress={() => this.changeTab('contact')}>
-              <Resume />
-          </Tab>
-          <Tab
-            tabStyle={selectedTab !== 'pricing' && { marginBottom: -6 }}
-            titleStyle={[styles.titleStyle, {marginTop: -1}]}
-            selectedTitleStyle={[styles.titleSelected, {marginTop: -3, marginBottom: 7}]}
-            selected={selectedTab === 'pricing'}
-            title={selectedTab === 'pricing' ? 'PRICING' : null}
-            renderIcon={() => <Icon style={{paddingBottom: 4}} color={'grey'} name='attach-money' size={26} />}
-            renderSelectedIcon={() => <Icon color={'blue'} name='attach-money' size={26} />}
-            onPress={() => this.changeTab('pricing')}>
-              <Resume />
-          </Tab>
+        );
+      });
+
+      return (
+        <Tabs hidesTabTouch>
+          {innerTabs}
         </Tabs>
       );
   }
