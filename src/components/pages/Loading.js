cc2af5946db90331195917b4a7f9ec9e21ad3ec8
gergo.bogdan@accedo.tv <greg@grelution.com>
2016-10-13 10:33:44 +0200

    added Loading page

---
+++ src/components/pages/Loading.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import  {
+    View,
+    Text,
+    Alert,
+    TextInput,
+    Image,
+    TouchableHighlight,
+    StyleSheet
+} from 'react-native';
+
+
+
+const Loading = React.createClass({
+    getInitialState() {
+        return {
+
+        }
+    },
+
+    render () {
+        return (
+            <Text>Loading</Text>
+        );
+    }
+});
+
+export default Loading;
