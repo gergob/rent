d50ad91d304d27411dfa1e1920e81752757a95d4
gergo.bogdan@accedo.tv <greg@grelution.com>
2016-10-14 12:50:01 +0200

    added react-native-player android support

--- android/app/src/main/java/com/rent/MainApplication.java
+++ android/app/src/main/java/com/rent/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.brentvatne.react.ReactVideoPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new ReactVideoPackage()
       );
     }
   };
