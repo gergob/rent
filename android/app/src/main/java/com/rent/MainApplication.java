ecdf50a1536bc80251693c9c97dcc6b3462f6d30
gergo.bogdan@accedo.tv <greg@grelution.com>
2017-02-14 23:51:37 +0100

    added GA for android

--- android/app/src/main/java/com/rent/MainApplication.java
+++ android/app/src/main/java/com/rent/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new GoogleAnalyticsBridgePackage(),
           new ReactVideoPackage(),
           new VectorIconsPackage()
       );
